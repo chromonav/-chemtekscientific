7aa39f4f9cd9b357b425ba43819b789521294929
DPriyanka123 <102216336+DPriyanka123@users.noreply.github.com>
2022-08-05 11:49:57 +0530

    Update contact_person_report_with_detail.py

--- chemtech_custom_app/chemtech/report/contact_person_report_with_detail/contact_person_report_with_detail.py
+++ chemtech_custom_app/chemtech/report/contact_person_report_with_detail/contact_person_report_with_detail.py
@@ -10,35 +10,16 @@ def execute(filters=None):
 
 def get_data(filters):
     if filters:
+        
         data = frappe.db.sql(""" SELECT DISTINCT cu.sales_person as sales_person_name,cu.customer_name as customer_name,co.address as contact_address,
             CONCAT_WS(' ' ,co.first_name,co.last_name) as contact_person,co.creation as creation_date,co.status as contact_status,co.designation as designation,co.department as department,
-            co.mobile_no as mobile_no,co.phone as landline,co.email_id as email_id,
+            co.mobile_no as mobile_no,co.phone as landline,co.email_id as email_id,co.area_of_interest_1 as area_of_interest_1,
             co.area_of_interest_2 as area_of_interest_2,co.hplc as hplc,co.uplc as uplc,co.gchs as gchs,co.gcms as gcms,
             co.lcms as lcms,co.icp_ as icp,co._kfr as kfr,co.ic as ic,co.ph_ as ph,co.icpms as icpms,co.ftir_ as ftir,
             co.dissolution as dissolution,co.malvern as malvern FROM tabCustomer cu JOIN tabContact co ON cu.customer_name=co.company_name
             WHERE co.first_name='{0}' OR co.status='{1}'  """
             .format(filters.get('first_name'),filters.get('status')),as_dict=1,debug=1)
 
-        address_list = [adrs.contact_address for adrs in data]
-        
-        data1 = frappe.db.sql("""SELECT name,city as ad_city,CONCAT_WS(' ' , address_line1,address_line2,city,state,gst_state_number,pincode,country)as full_address,state as ad_state FROM tabAddress""",as_dict=1,debug=1)
-        add_list = []
-        for i in address_list:
-            if i != None:
-                add_list.append(i)
-        for row in data:
-            for row1 in data1:
-                if row.get('contact_address') == row1.get('name'):
-                    row.update(row1)
-        
-        return data
-    else:
-        data =frappe.db.sql(""" SELECT DISTINCT cu.sales_person as sales_person_name,cu.customer_name as customer_name,co.address as contact_address,
-            CONCAT_WS(' ' ,co.first_name,co.last_name) as contact_person,co.creation as creation_date,co.status as contact_status,co.designation as designation,co.department as department,
-            co.mobile_no as mobile_no,co.phone as landline,co.email_id as email_id,
-            co.area_of_interest_2 as area_of_interest_2,co.hplc as hplc,co.uplc as uplc,co.gchs as gchs,co.gcms as gcms,
-            co.lcms as lcms,co.icp_ as icp,co._kfr as kfr,co.ic as ic,co.ph_ as ph,co.icpms as icpms,co.ftir_ as ftir,
-            co.dissolution as dissolution,co.malvern as malvern  FROM tabCustomer cu JOIN tabContact co ON cu.customer_name=co.company_name""",as_dict=1,debug=1)
         address_list = [adrs.contact_address for adrs in data]
         
         data1 = frappe.db.sql("""SELECT name,city as ad_city,CONCAT_WS(' ' , address_line1,address_line2)as full_address,state as ad_state FROM tabAddress""",as_dict=1,debug=1)
@@ -51,7 +32,31 @@ def get_data(filters):
             for row1 in data1:
                 if row.get('contact_address') == row1.get('name'):
                     row.update(row1)
-                
+        
+        return data
+    else:
+        data =frappe.db.sql(""" SELECT DISTINCT cu.sales_person as sales_person_name,cu.customer_name as customer_name,co.address as contact_address,
+            CONCAT_WS(' ' ,co.first_name,co.last_name) as contact_person,co.creation as creation_date,co.status as contact_status,co.designation as designation,co.department as department,
+            co.mobile_no as mobile_no,co.phone as landline,co.email_id as email_id,co.area_of_interest_1 as area_of_interest_1,
+            co.area_of_interest_2 as area_of_interest_2,co.hplc as hplc,co.uplc as uplc,co.gchs as gchs,co.gcms as gcms,
+            co.lcms as lcms,co.icp_ as icp,co._kfr as kfr,co.ic as ic,co.ph_ as ph,co.icpms as icpms,co.ftir_ as ftir,
+            co.dissolution as dissolution,co.malvern as malvern  FROM tabCustomer cu JOIN tabContact co ON cu.customer_name=co.company_name""",as_dict=1,debug=1)
+        
+        address_list = [adrs.contact_address for adrs in data]
+        
+        
+        data1 = frappe.db.sql("""SELECT name,city as ad_city,CONCAT_WS(' ' , address_line1,address_line2,city,state,gst_state_number,pincode,country)as full_address,state as ad_state FROM tabAddress""",as_dict=1,debug=1)
+                add_list = []
+        for i in address_list:
+            if i != None:
+                add_list.append(i)
+        
+        for row in data:
+            for row1 in data1:
+                if row.get('contact_address') == row1.get('name'):
+                    row.update(row1)
+                #print("in row",row)            
+
         return data
 
 def get_columns(filters):
@@ -142,12 +147,12 @@ def get_columns(filters):
             "fieldtype": "Data",
             "width": 80,
         },	
-        #{
-        #   "label": _("Area of Interest 1"),
-        #    "fieldname": "area_of_interest_1",
-        #   "fieldtype": "Data",
-        #    "width": 80,
-        #},
+        # {
+        #     "label": _("Area of Interest 1"),
+        #     "fieldname": "area_of_interest_1",
+        #     "fieldtype": "Data",
+        #     "width": 80,
+        # },
         {
             "label": _("Area of Interest 2"),
             "fieldname": "area_of_interest_2",
@@ -239,31 +244,3 @@ def get_columns(filters):
 
 
 
-# def get_data(filters):
-#     if filters:
-#         print("++++++++++++++++++",filters)
-#         data = frappe.db.sql(""" SELECT cu.sales_person as sales_person_name, cu.customer_name as customer_name,
-#             co.address as full_addess,ad.city as ad_city,ad.state as ad_state,CONCAT(co.first_name,co.last_name) as contact_person,co.creation as creation_date,
-#             co.status as contact_status,co.designation as designation,co.department as department,co.mobile_no as mobile_no,
-#             co.phone as landline,co.email_id as email_id,co.area_of_interest_1 as area_of_interest_1,
-#             co.area_of_interest_2 as area_of_interest_2,co.hplc as hplc,co.uplc as uplc,co.gchs as gchs,co.gcms as gcms,co.lcms as lcms,
-#             co.icp_ as icp,co._kfr as kfr,co.ic as ic,co.ph_ as ph,co.icpms as icpms,co.ftir_ as ftir,co.dissolution as dissolution,co.malvern as malvern
-#             FROM `tabCustomer` cu JOIN `tabContact`co ON cu.customer_name=co.company_name JOIN tabAddress ad ON ad.address_title=co.address
-#             WHERE co.first_name ='{0}' OR co.status='{1}' """.format(filters.get('first_name'),filters.get('status'),as_dict=1,debug=1))
-#         print("~~~~~~~~~~~~~~~~~~~~~~~~",data)
-#         return data
-#     # elif filters:
-#     #     print("======= in elif",filters)
-#     #     data = frappe.db.sql(""" SELECT cu.sales_person as sales_person_name, cu.customer_name as customer_name,co.address as full_addess,CONCAT(co.first_name,co.last_name) as contact_person,co.creation as creation_date,co.status as contact_status,co.designation as designation FROM `tabCustomer` cu JOIN `tabContact`co ON cu.customer_name=co.company_name WHERE cu.sales_person ='{0}' OR co.status='{1}' """.format(filters.get('sales_person'),filters.get('status'),as_dict=1,debug=1))
-#     #     return data
-#     else:
-#         print("---------------in else")
-#         data = frappe.db.sql("""SELECT cu.sales_person as sales_person_name,CONCAT(co.first_name,co.last_name) as contact_person,
-#             co.company_name as customer_name,co.address as full_address,ad.city as ad_city,ad.state as ad_state,co.creation as creation_date,co.status as contact_status,
-#             co.designation as designation,co.department as department,co.mobile_no as mobile_no,
-#             co.phone as landline,co.email_id as email_id,co.area_of_interest_1 as area_of_interest_1,
-#             co.area_of_interest_2 as area_of_interest_2,co.hplc as hplc,co.uplc as uplc,co.gchs as gchs,co.gcms as gcms,co.lcms as lcms,
-#             co.icp_ as icp,co._kfr as kfr,co.ic as ic,co.ph_ as ph,co.icpms as icpms,co.ftir_ as ftir,co.dissolution as dissolution,co.malvern as malvern
-#             FROM `tabContact` co JOIN `tabCustomer` cu ON co.company_name=cu.customer_name
-#             JOIN tabAddress ad ON ad.address_title=co.address""",as_dict=1,debug=1)
-#         return data
