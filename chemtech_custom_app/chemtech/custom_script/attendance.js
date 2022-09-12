cc9a824d07b4c07f3a9669452f67f464d6b77d5e
Priyanka1413 <priyanka.d@indictrans.in>
2022-09-12 19:52:43 +0530

    added modified code

--- chemtech_custom_app/chemtech/custom_script/attendance.js
+++ chemtech_custom_app/chemtech/custom_script/attendance.js
@@ -1,27 +1,33 @@
 frappe.ui.form.on('Attendance',  {
     validate: function(frm) {
         if(frappe.user_roles.includes('Employee Checkin')) {
+             frm.set_df_property('attendance_date', 'read_only', 1);
             //msgprint('You are only allowed Checkin');
             if (frm.doc.in) {
                 frm.set_df_property('start_time', 'read_only', 1);
                 frm.set_df_property('in', 'read_only', 1);
 
             }
+            if(!frm.doc.out){
+                frm.set_df_property('end_time','hidden',1);
+            }
             if (frm.doc.out) {
+                frm.set_df_property('end_time','hidden',0);
                 frm.set_df_property('end_time', 'read_only', 1);
                 frm.set_value('end_time', frappe.datetime.now_time());
                 frm.set_df_property('out', 'read_only', 1);
 
             }
-            //validated = false;
+            
         }
         if(frappe.session.user === 'Administrator') {
+            frm.set_df_property('attendance_date', 'read_only', frm.doc.__islocal ? 0 : 1);
             //msgprint('You are only allowed edit doc');
-            frm.set_df_property('start_time', 'read_only', 0);
+            /*frm.set_df_property('start_time', 'read_only', 0);
             frm.set_df_property('in', 'read_only', 0);
             frm.set_df_property('end_time', 'read_only', 0);
             frm.set_df_property('out', 'read_only', 0);
-            validated = true;
+            */
         }
 
     },
@@ -29,14 +35,16 @@ frappe.ui.form.on('Attendance',  {
     if(frm.doc.shift)
         {
             console.log("&&&&&&&&&in shift&&&&",cur_frm.doc.shift);
-            if(frm.doc.shift)
+            if(frm.doc.start_time)
             {
+                console.log("##",frm.doc.start_time);
                 frappe.call({
                         method: 'chemtech_custom_app.chemtech.custom_script.attendance.get_shift_time_detail',
                         args: {
                             //shift: frm.doc.shift,
                             start_time:cur_frm.doc.start_time,
                             doc:cur_frm.doc
+
                         },
                         callback:function(r){
                             if(r.message)                            
--- chemtech_custom_app/chemtech/custom_script/attendance.py
+++ chemtech_custom_app/chemtech/custom_script/attendance.py
@@ -20,56 +20,22 @@ from datetime import datetime, date
 # 		return early_by_time,late_by_time
 
 
-def get_shift_time_detail(doc):
-	print("~~~~~~~~~~~~~~~~doc",doc)
-	doc = json.loads(doc)
-	print("dddddddddoc = ",doc)
-	early_by_time=0
-	late_by_time=0
-	shift = doc.get('shift')
-	#start_time = doc.get('start_time')
-	shift_start_time = frappe.db.get_value("Shift Type",{'name':shift},"start_time")
-	shift_start_time2 = datetime.strptime(str(shift_start_time),'%H:%M:%S').time()
-	frm_start_time = datetime.strptime(doc.get('start_time'), '%H:%M:%S').time()
-	
-	if frm_start_time < shift_start_time2:
-		print("*****************",shift_start_time2,frm_start_time)
-		early_by_time = datetime.combine(date.today(), shift_start_time2) - datetime.combine(date.today(), frm_start_time)
-		print("++++++++++++early_by_time",early_by_time)
-		return early_by_time,late_by_time
-
-	if frm_start_time > shift_start_time2:
-		print("*****************",shift_start_time2,frm_start_time)
-		late_by_time = datetime.combine(date.today(), frm_start_time) - datetime.combine(date.today(), shift_start_time2)
-		print("++++++++++++early_by_time",early_by_time)
-
-		return early_by_time,late_by_time
-
-
-# @frappe.whitelist()
-# def get_shift_time_detail(start_time,doc):
-# 	print("##########",doc)
+# def get_shift_time_detail(doc):
+# 	print("~~~~~~~~~~~~~~~~doc",doc)
 # 	doc = json.loads(doc)
+# 	print("dddddddddoc = ",doc)
 # 	early_by_time=0
 # 	late_by_time=0
-# 	#start_time = doc.get('start_time')
 # 	shift = doc.get('shift')
-# 	# datetime_obj = parser.parse('start_time')
-# 	# #print datetime_obj
-# 	# print("@@@@@@@@@",datetime_obj)
-
-# 	frm_start_time = datetime.strptime(doc.get('start_time'), '%H:%M:%S').time()
-
+# 	#start_time = doc.get('start_time')
 # 	shift_start_time = frappe.db.get_value("Shift Type",{'name':shift},"start_time")
 # 	shift_start_time2 = datetime.strptime(str(shift_start_time),'%H:%M:%S').time()
-
-# 	print("~~~~~~~~~~~~~~~~~~",type(shift_start_time2),type(frm_start_time))
-
+# 	frm_start_time = datetime.strptime(doc.get('start_time'), '%H:%M:%S').time()
+	
 # 	if frm_start_time < shift_start_time2:
 # 		print("*****************",shift_start_time2,frm_start_time)
 # 		early_by_time = datetime.combine(date.today(), shift_start_time2) - datetime.combine(date.today(), frm_start_time)
 # 		print("++++++++++++early_by_time",early_by_time)
-
 # 		return early_by_time,late_by_time
 
 # 	if frm_start_time > shift_start_time2:
@@ -78,3 +44,37 @@ def get_shift_time_detail(doc):
 # 		print("++++++++++++early_by_time",early_by_time)
 
 # 		return early_by_time,late_by_time
+
+
+@frappe.whitelist()
+def get_shift_time_detail(start_time,doc):
+	print("##########",doc)
+	doc = json.loads(doc)
+	early_by_time=0
+	late_by_time=0
+	#start_time = doc.get('start_time')
+	shift = doc.get('shift')
+	# datetime_obj = parser.parse('start_time')
+	# #print datetime_obj
+	# print("@@@@@@@@@",datetime_obj)
+
+	frm_start_time = datetime.strptime(doc.get('start_time') , '%H:%M:%S.%f').time()
+
+	shift_start_time = frappe.db.get_value("Shift Type",{'name':shift},"start_time")
+	shift_start_time2 = datetime.strptime(str(shift_start_time),'%H:%M:%S').time()
+
+	print("~~~~~~~~~~~~~~~~~~",type(shift_start_time),type(start_time),frm_start_time)
+
+	if frm_start_time < shift_start_time2:
+		print("*****************",shift_start_time2,frm-start_time)
+		early_by_time = datetime.combine(date.today(), shift_start_time2) - datetime.combine(date.today(), frm_start_time)
+		print("++++++++++++early_by_time",early_by_time)
+
+		return early_by_time,late_by_time
+
+	if frm_start_time > shift_start_time2:
+		print("*****************",shift_start_time2,frm_start_time)
+		late_by_time = datetime.combine(date.today(), frm_start_time) - datetime.combine(date.today(), shift_start_time2)
+		print("++++++++++++early_by_time",early_by_time)
+
+		return early_by_time,late_by_time
