277a0403b427869a09a93d3cb1ad3de65d5429fb
Priyanka1413 <priyanka.d@indictrans.in>
2022-09-13 15:17:03 +0530

    added edited script

--- chemtech_custom_app/chemtech/custom_script/attendance.js
+++ chemtech_custom_app/chemtech/custom_script/attendance.js
@@ -1,6 +1,9 @@
 frappe.ui.form.on('Attendance',  {
+    //refresh:function(frm){
     validate: function(frm) {
-        if(frappe.user_roles.includes('Employee Checkin')) {
+        console.log("in js");
+        
+        if(frappe.user_roles.includes('Employee Checkin') && frappe.session.user != 'Administrator') {
              frm.set_df_property('attendance_date', 'read_only', 1);
             //msgprint('You are only allowed Checkin');
             if (frm.doc.in) {
@@ -20,18 +23,19 @@ frappe.ui.form.on('Attendance',  {
             }
             
         }
-        if(frappe.session.user === 'Administrator') {
-            frm.set_df_property('attendance_date', 'read_only', frm.doc.__islocal ? 0 : 1);
+        
+
+    
+    
+        /*if(frappe.session.user == 'Administrator') {
+            cur_frm.set_df_property('attendance_date','read_only',0);
             //msgprint('You are only allowed edit doc');
-            /*frm.set_df_property('start_time', 'read_only', 0);
+            frm.set_df_property('start_time', 'read_only', 0);
             frm.set_df_property('in', 'read_only', 0);
             frm.set_df_property('end_time', 'read_only', 0);
             frm.set_df_property('out', 'read_only', 0);
-            */
-        }
-
-    },
-    refresh:function(frm){
+            
+        }*/
     if(frm.doc.shift)
         {
             console.log("&&&&&&&&&in shift&&&&",cur_frm.doc.shift);
@@ -64,3 +68,65 @@ frappe.ui.form.on('Attendance',  {
         }
     }
 });
+
+/*
+frappe.ui.form.on('Attendance',  {
+    validate: function(frm) {
+        if(frappe.user_roles.includes('Employee Checkin')) {
+            //msgprint('You are only allowed Checkin');
+            frm.set_df_property('attendance_date', 'read_only', 1);
+            if (frm.doc.in) {
+                frm.set_df_property('start_time', 'read_only', 1);
+                frm.set_df_property('in', 'read_only', 1);
+
+            }
+            if (frm.doc.out) {
+                frm.set_df_property('end_time', 'read_only', 1);
+                frm.set_value('end_time', frappe.datetime.now_time());
+                frm.set_df_property('out', 'read_only', 1);
+
+            }
+            //validated = false;
+        }
+        if(frappe.session.user === 'Administrator') {
+            //msgprint('You are only allowed edit doc');
+            frm.set_df_property('start_time', 'read_only', 0);
+            frm.set_df_property('in', 'read_only', 0);
+            frm.set_df_property('end_time', 'read_only', 0);
+            frm.set_df_property('out', 'read_only', 0);
+            //validated = true;
+        }
+
+    },
+    refresh:function(frm){
+    if(frm.doc.shift)
+        {
+            console.log("&&&&&&&&&in shift&&&&",cur_frm.doc.shift);
+            if(frm.doc.shift)
+            {
+                frappe.call({
+                        method: 'chemtech_custom_app.chemtech.custom_script.attendance.get_shift_time_detail',
+                        args: {
+                            //shift: frm.doc.shift,
+                            start_time:cur_frm.doc.start_time,
+                            doc:cur_frm.doc
+                        },
+                        callback:function(r){
+                            if(r.message)                            
+                            {
+                                console.log(r)
+                                cur_frm.set_value("early_by",r.message[0]);
+
+                                console.log("in early_by",cur_frm.doc.early_by)
+                                cur_frm.set_value("late_by",r.message[1]);
+                                console.log("in late_by",cur_frm.doc.late_by)
+                                //refresh_field("early_by");
+                            
+                            }
+                        }
+                    });
+            }
+        }
+    }
+});
+*/
