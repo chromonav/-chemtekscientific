78d128dd4bfcdd17727424e7839e8fee06803b65
DPriyanka123 <102216336+DPriyanka123@users.noreply.github.com>
2022-09-26 10:24:57 +0530

    Update product_wise_profitability_report.py

--- chemtech_custom_app/chemtech/report/product_wise_profitability_report/product_wise_profitability_report.py
+++ chemtech_custom_app/chemtech/report/product_wise_profitability_report/product_wise_profitability_report.py
@@ -48,8 +48,8 @@ def get_data(filters):
     data2 = frappe.db.sql(""" SELECT DISTINCT pii.item_code,po.name as purchase_order,po.company,po.transaction_date,pii.item_name, pii.rate as pi_rate, pii.qty as pii_qty, 
     pii.taxable_value as po_taxable_value,po.company,pii.item_tax_template as pii_item_tax_template,
     po.total_taxes_and_charges as po_gst_tax_amount,(pii.taxable_value+po.total_taxes_and_charges) as pi_total_value from `tabPurchase Invoice Item` pii 
-    join `tabPurchase Order` po  on pii.purchase_order=po.name join `tabSales Invoice` si  on po.company=si.company where pii.item_code in {0} and 
-    po.company='{1}'and po.transaction_date<si.posting_date """.format(tuple(item_code_list),filters.get('company')),as_dict=1,debug=1)
+    join `tabPurchase Order` po  on pii.purchase_order=po.name join `tabSales Invoice` si  on po.company=si.company where po.company='{1}'
+     and  pii.item_code in {0} and po.transaction_date<si.posting_date """.format(tuple(item_code_list),filters.get('company')),as_dict=1,debug=1)
     
 
     for row in data:
