ebccd6069fa026ac901b00957badd966da2cae46
DPriyanka123 <priyanka.d@indictrans.in>
2022-12-05 18:12:34 +0530

    Added pr,pi,stock opening/transfer code in report

--- chemtech_custom_app/chemtech/report/product_wise_profitability_report/product_wise_profitability_report.py
+++ chemtech_custom_app/chemtech/report/product_wise_profitability_report/product_wise_profitability_report.py
@@ -34,11 +34,11 @@ def validate_filters(filters):
 
 
 def get_data(filters):
-    data = frappe.db.sql(""" SELECT DISTINCT si.posting_date as si_posting_date,MONTHNAME(si.posting_date) as posting_date_month,si.company,si.name as sales_invoice_name,si.customer_name,
+    data = frappe.db.sql(""" SELECT DISTINCT si.posting_date,MONTHNAME(si.posting_date) as posting_date_month,si.company,si.name as sales_invoice_name,si.customer_name,
     ad.city as location,sii.item_code as item_code,sii.brand as si_brand,sii.lot_number as lot_no,sii.item_name,
     sii.rate as si_rate,sii.item_tax_template as sii_item_tax_template,sii.qty as si_qty,sii.lot_number as lot_no,
     sii.taxable_value as si_taxable_value,si.total_taxes_and_charges as si_gst_tax_amount,sii.batch_no as si_batch_no
-    FROM `tabSales Invoice` si JOIN `tabSales Invoice Item` sii on sii.parent = si.name
+     FROM `tabSales Invoice` si JOIN `tabSales Invoice Item` sii on sii.parent = si.name
     JOIN `tabAddress` ad ON si.customer_address = ad.name WHERE posting_date BETWEEN '{0}' and '{1}' and si.company='{2}' and si.status not in ('Rerurn','Cancelled')"""
     .format(filters.get('from_date'), filters.get('to_date'),filters.get('company')),as_dict=1,debug=1)
     item_code_list = [item.item_code for item in data]
@@ -53,8 +53,7 @@ def get_data(filters):
      where  pii.item_code in {0} and pi.posting_date < si.posting_date  and pi.company= '{1}'
      and pi.status not in ('Cancelled', 'Return','Draft','Debit Note Issued')
      """.format(tuple(item_code_list),filters.get('company')),as_dict=1,debug=1)
-    
-     
+
     data2 = frappe.db.sql(""" SELECT DISTINCT 
         sri.parent as stock_voucher,sr.purpose as stock_purpose,sr.posting_date ,sri.item_code,
         sri.batch_no,sri.valuation_rate as valuation_rate,sri.qty
@@ -79,58 +78,62 @@ def get_data(filters):
      where sed.item_code in {0} and se.company= '{1}' and se.posting_date < si.posting_date
         """.format(tuple(item_code_list),filters.get('company')),as_dict=1,debug=1)
     
-    si_gst_list = []
-    si_test_list=[]
-    si_Final_gst=[]
-    for row in data:
-        if (row.get('sii_item_tax_template')!=None):
-            si_temp = re.findall(r'\d+', row.get('sii_item_tax_template'))
-            si_res = list(map(int, si_temp))
-            si_gst_list.append(si_res)
-            for i in si_gst_list:
-                si_test_list = [str(element) for element in i]  
-                 
-                si_new_integer = int(''.join(si_test_list))
-                si_Final_gst.append(si_new_integer)
-                for i in si_Final_gst:
-                    si_qty_rate = (row.get('si_rate')*row.get('si_qty'))
-                    row.update({'si_gst_tax_amount':(si_qty_rate)*flt(i/100)})
-                    row.update({'si_total_value':(flt(row.get('si_taxable_value')+row.get('si_gst_tax_amount')))})
 
-
-    pi_list=[]
-    pi_gst_list =[]
+    #----------- get tax int value for PI starts-------------- 
+    pi_gst_list = []
+    pi_test_list=[]
     pi_Final_gst=[]
-    for row in data:
-        for row1 in data1:
-            if (row1.get('pi_tax')!=None):
-                pi_temp = re.findall(r'\d+', row1.get('pi_tax'))
-                pi_res = list(map(int, pi_temp))
-                pi_gst_list.append(pi_res)
-                for i in pi_gst_list:
-                    pi_list = [str(element) for element in i]  
-                    pi_new_integer = int(''.join(pi_list))
-                    pi_Final_gst.append(pi_new_integer)
-                    # for i in pi_Final_gst:
-                    #     pur_inv_qty_rate = (row1.get('pi_rate')*row.get('si_qty'))
-                    #     row.update({'pi_gst_tax_amount':(pur_inv_qty_rate)*flt(i/100)})
-                    
+    for row1 in data1:
+        if (row1.get('pi_tax')!=None):
+            pi_temp = re.findall(r'\d+', row1.get('pi_tax'))
+            pi_res = list(map(int, pi_temp))
+            pi_gst_list.append(pi_res)
+            for i in pi_gst_list:
+                pi_test_list = [str(element) for element in i]  
+                #pi_new_integer = int(''.join(pi_test_list))
+                pi_Final_gst.append(pi_test_list)
+                
+    #----------- get tax int value ends-------------- 
+
+    #----------- get tax int value for PR starts-------------- 
+    pr_gst_list = []
+    pr_test_list=[]
+    pr_Final_gst=[]
+    for row3 in data3:
+        if (row3.get('pr_tax')!=None):
+            pr_temp = re.findall(r'\d+', row3.get('pr_tax'))
+            pr_res = list(map(int, pr_temp))
+            pr_gst_list.append(pr_res)
+            for i in pr_gst_list:
+                pr_test_list = [str(element) for element in i]  
+                #pi_new_integer = int(''.join(pi_test_list))
+                pr_Final_gst.append(pr_test_list)
+                
+    #----------- get tax int value ends-------------- 
+
 
     for row in data:
         row.update({'si_total_value':(flt(row.get('si_taxable_value')+row.get('si_gst_tax_amount')))})
-        
         for row1 in data1:
-            if (row.get('item_code')==row1.get('item_code')and row.get('company') == row1.get('company')):
-                row.update({'purchase_taxable_value':(flt(row1.get('pi_rate')*row.get('si_qty')))})
-                #row.update({'pi_gst_tax_amount':row.get('pi_gst_tax_amount')})
-                    
-                row.update({'pi_total_value':(flt(row.get('purchase_taxable_value')+flt(row.get('pi_gst_tax_amount'))))})
-                #row.update({'gross_profit':(flt(row.get('si_total_value')-row.get('pi_total_value')))})
-                        
-                row.update(row1)
-                
-        for row2 in data2:
-            if row.get('purchase_invoice') == None :
+            if row.get('item_code') == row1.get('item_code') and row.get('company')==row1.get('company'):
+                row.update({'purchase_invoice':(row1.get('purchase_invoice'))})
+                row.update({'pi_rate':(row1.get('pi_rate'))})
+                row.update({'purchase_taxable_value':(row1.get('pi_rate')*(row.get('si_qty')))})
+                row.update({'pi_tax':(row1.get('pi_tax'))})
+                if row.get('pi_tax')!=None:
+                    for i in pi_gst_list:
+                        for j in i:
+                            row.update({'pi_gst_tax_amount':(int(row.get('purchase_taxable_value')*flt(j/100)))})
+                    row.update({'pi_total_value':(flt(row.get('purchase_taxable_value')+row.get('pi_gst_tax_amount')))})
+                    row.update({'gross_profit':(flt(row.get('si_total_value')-row.get('pi_total_value')))})
+                else:
+                    #print("---no pi_tax-----")
+                    row.update({'pi_total_value':(flt(row.get('purchase_taxable_value')))})
+                    row.update({'gross_profit':(flt(row.get('si_total_value')-row.get('pi_total_value')))})
+        
+        if row.get('purchase_invoice') == None :
+            #print("------empty pi------------")
+            for row2 in data2: 
                 if (row.get('item_code')==row2.get('item_code')):
                     row.update({'purchase_invoice':row2.get('stock_voucher')})
                     row.update({'stock_purpose':row2.get('stock_purpose')})
@@ -138,36 +141,36 @@ def get_data(filters):
                     row.update({'purchase_taxable_value':(flt(row.get('pi_rate')*row.get('si_qty')))})                            
                     row.update({'pi_total_value':(flt(row.get('pi_rate')*row.get('si_qty')))})                        
                     row.update({'gross_profit':(flt(row.get('si_total_value')-row.get('purchase_taxable_value')))})
-                      
-
-        for row3 in data3:
-            if row.get('purchase_invoice') == None and row.get('stock_voucher')==None:
-                if (row.get('item_code')==row3.get('item_code')):
-                    row.update({'purchase_invoice':row3.get('purchase_receipt')})
-                    row.update({'pi_rate':row3.get('purchase_rec_rate')})
-                    row.update({'pi_tax':row3.get('pr_tax')})
-                    row.update({'purchase_taxable_value':(flt(row.get('pi_rate')*row.get('si_qty')))})
-                    row.update({'pi_total_value':(flt(row.get('purchase_taxable_value')+row3.get('pr_gst_tax_amount')))})
-                    row.update({'pi_gst_tax_amount':row3.get('pr_gst_tax_amount')})
-                    row.update({'pi_total_value':(flt(row.get('purchase_taxable_value')+row.get('pi_gst_tax_amount')))})
-                    row.update({'gross_profit':(flt(row.get('si_total_value')-row.get('pi_total_value')))})
-
-        for row4 in data4:
-            if row.get('purchase_invoice') == None and row.get('stock_voucher')==None and row.get('purchase_receipt')==None :
-                if (row.get('item_code')==row4.get('item_code')):
-                    row.update({'purchase_invoice':row4.get('manufacture')})
-                    row.update({'stock_purpose':row4.get('stock_entry_type')})
-                    row.update({'pi_rate':row4.get('stock_rate')})
-                    row.update({'purchase_taxable_value':(flt(row.get('pi_rate')*row.get('si_qty')))})
-                    row.update({'pi_total_value':(flt(row.get('pi_rate')*row.get('si_qty')))}) 
-                    row.update({'gross_profit':(flt(row.get('si_total_value')-row.get('purchase_taxable_value')))})
+        
+            for row3 in data3:
+                if row.get('purchase_invoice') == None and row.get('stock_voucher')==None:
+                    if (row.get('item_code')==row3.get('item_code')):
+                        row.update({'purchase_invoice':row3.get('purchase_receipt')})
+                        row.update({'stock_purpose':'Purchase Receipt'})
+                        row.update({'pi_rate':row3.get('purchase_rec_rate')})
+                        row.update({'pi_tax':row3.get('pr_tax')})
+                        row.update({'purchase_taxable_value':(flt(row.get('pi_rate')*row.get('si_qty')))})
+                        if row3.get('pr_tax')!=None:
+                            print("-------in pr_tax != None--------")
+                            for i in pr_gst_list:
+                                for j in i:
+                                    print("--------IN pi_gst_list J==---------")
+                                    row.update({'pi_gst_tax_amount':(int(row.get('purchase_taxable_value')*flt(j/100)))})
+                            row.update({'pi_total_value':(flt(row.get('purchase_taxable_value')+row.get('pi_gst_tax_amount')))})
+                        row.update({'gross_profit':(flt(row.get('si_total_value')-row.get('pi_total_value')))})
+        
+            for row4 in data4:
+                if row.get('purchase_invoice') == None  :
+                    if (row.get('item_code')==row4.get('item_code')):
+                        row.update({'purchase_invoice':row4.get('manufacture')})
+                        row.update({'stock_purpose':row4.get('stock_entry_type')})
+                        row.update({'pi_rate':row4.get('stock_rate')})
+                        row.update({'purchase_taxable_value':(flt(row.get('pi_rate')*row.get('si_qty')))})
+                        row.update({'pi_total_value':(flt(row.get('pi_rate')*row.get('si_qty')))}) 
+                        row.update({'gross_profit':(flt(row.get('si_total_value')-row.get('purchase_taxable_value')))})
            
-    return data
-
-
-
     
-    
+    return data 
 
 def get_columns(filters):
     columns = [
@@ -182,13 +185,12 @@ def get_columns(filters):
         {
             "label": _("Sales Invoice"),
             "fieldname": "sales_invoice_name",
-            "fieldtype": "Link",
-            "options":"Sales Invoice",
+            "fieldtype": "Data",
             "width": 80,
         },
         {
             "label": _("Sell Date"),
-            "fieldname": "si_posting_date",
+            "fieldname": "posting_date",
             "fieldtype": "Date",
             "width": 80,
         },
@@ -217,7 +219,7 @@ def get_columns(filters):
             "width": 80,
         }, 
         {
-            "label": _("Stock Purpose"),
+            "label": _("Stock Opening/Transfer"),
             "fieldname": "stock_purpose",
             "fieldtype": "Data",
             "width": 80,
@@ -228,17 +230,13 @@ def get_columns(filters):
             "fieldtype": "Data",
             "width": 80,
         },
+        
+        
         {
             "label": _("Item Name"),
             "fieldname": "item_name",
             "fieldtype": "Data",
             "width": 80,
-        },
-        {
-            "label": _("Batch_NO"),
-            "fieldname": "si_batch_no",
-            "fieldtype": "Data",
-            "width": 50,
         },
         {
             "label": _("Sell Qt"),
@@ -252,7 +250,12 @@ def get_columns(filters):
             "fieldtype": "Data",
             "width": 50,
         },
-        
+        #{
+        #     "label": _("Batch_NO"),
+        #     "fieldname": "si_batch_no",
+        #     "fieldtype": "Data",
+        #     "width": 50,
+        #},
         # {
         #     "label": _("Lot No"),
         #     "fieldname": "lot_no",
@@ -330,7 +333,7 @@ def get_columns(filters):
         
         {
             "label": _("Purchase Total Value"),
-            "fieldname": "pi_total",
+            "fieldname": "pi_total_value",
             "fieldtype": "Currency",
             "width": 80,
         },
