e6169373448e38067364a69d82e633f3d2249397
DPriyanka123 <102216336+DPriyanka123@users.noreply.github.com>
2023-01-16 15:52:55 +0530

    Update contact_person_report_with_detail.py

--- chemtech_custom_app/chemtech/report/contact_person_report_with_detail/contact_person_report_with_detail.py
+++ chemtech_custom_app/chemtech/report/contact_person_report_with_detail/contact_person_report_with_detail.py
@@ -36,7 +36,7 @@ def get_data(filters):
             CONCAT_WS(' ' ,co.first_name,co.last_name) as contact_person,co.creation as creation_date,co.status as contact_status,
             co.salutation as contact_salutation,co.designation as designation,co.department as department,co.mobile_no as mobile_no,co.phone as landline,co.email_id as email_id, co.area_of_interest_2 as area_of_interest_2,co.hplc as hplc,co.uplc as uplc,co.gchs as gchs,co.gcms as gcms,co.lcms as lcms,co.icp_ as icp,co._kfr as kfr,co.ic as ic,co.ph_ as ph,co.icpms as icpms,co.ftir_ as ftir,co.dissolution as dissolution,co.malvern as malvern  
             FROM tabContact co  JOIN `tabDynamic Link` dl  on co.name=dl.parent""",as_dict=1,debug=1)
-        #print("in data--------------------",data)
+        
         address_list = [adrs.contact_address for adrs in data]
         
         data1 = frappe.db.sql("""SELECT name,city as ad_city,CONCAT_WS(' ' , address_line1,address_line2,city,state,gst_state_number,pincode,country)as full_address,state as ad_state FROM tabAddress""",as_dict=1,debug=1)
