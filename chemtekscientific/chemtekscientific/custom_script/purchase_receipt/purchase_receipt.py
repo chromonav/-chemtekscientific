388af62bd0015028f3bcf93594b6d89b4c5fff43
subrotomivi <subroto.bera@seminoleelectronics.com>
2023-10-23 14:48:15 +0530

    [Feature][add a validation on purchase receipt and purchase invoice]

---
+++ chemtekscientific/chemtekscientific/custom_script/purchase_receipt/purchase_receipt.py
@@ -0,0 +1,7 @@
+import frappe
+
+def validate_supplier_delivery_note(doc, method=None):
+    if doc.supplier_delivery_note and doc.supplier:
+        data = frappe.get_value("Purchase Receipt", {'supplier_delivery_note' : doc.supplier_delivery_note, 'supplier' : doc.supplier}, ['name'])
+        if data:    
+            frappe.throw('Supplier sales invoice number already exits')
