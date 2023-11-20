0e2650e0e36cbec0763c03416bf70baca307cb13
subrotomivi <subroto.bera@seminoleelectronics.com>
2023-11-20 18:48:23 +0530

    [issue][validation issue fix]

--- chemtech_custom_app/chemtech/custom_script/purchase_receipt/purchase_receipt.py
+++ chemtech_custom_app/chemtech/custom_script/purchase_receipt/purchase_receipt.py
@@ -2,6 +2,8 @@ import frappe
 
 def validate_supplier_delivery_note(doc, method=None):
     if doc.supplier_delivery_note and doc.supplier and doc.docstatus != 2 and not doc.amended_from:      
-        data = frappe.get_value("Purchase Receipt", {'supplier_delivery_note' : doc.supplier_delivery_note, 'supplier' : doc.supplier}, ['name'])
-        if data:    
+        pr_data = frappe.db.get_value("Purchase Receipt", {'supplier_delivery_note' : doc.supplier_delivery_note, 'supplier' : doc.supplier}, ['name', 'docstatus', 'naming_series'], as_dict = 1)
+        if pr_data and pr_data.naming_series != doc.naming_series:
+            pass
+        elif pr_data and pr_data.docstatus != 2:    
             frappe.throw('Supplier sales invoice number already exits')
