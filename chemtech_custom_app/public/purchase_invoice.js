b1be6c0d68cbe67a2ddd91fea45ebe2aa06dda75
Pragati <pragati@mail.hybrowlabs.com>
2024-12-26 23:21:29 +0530

    |chemtech|added customization in purchase invoice and supplier|

--- chemtech_custom_app/public/purchase_invoice.js
+++ chemtech_custom_app/public/purchase_invoice.js
@@ -3,12 +3,12 @@ frappe.ui.form.on('Purchase Invoice', {
     on_save: function(frm) {
         $.each(frm.doc.items, function(index, item) {
             let exists = frm.doc.custom_additional_duties_and_charges.some(function(row) {
-                return row.item === item.item_name;
+                return row.item === item.item_code;
             });
 
             if (!exists) {
                 let row = frm.add_child("custom_additional_duties_and_charges");
-                row.item = item.item_name;  
+                row.item = item.item_code;  
                 row.duty_charges = 0;  
                 row.freight_charges = 0;  
                 row.other_charges = 0;  
