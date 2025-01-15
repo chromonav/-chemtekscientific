7d2fc66553b797222b20bcb4096d557b0a595605
Pragati <pragati@mail.hybrowlabs.com>
2025-01-15 15:00:44 +0530

    |chemtech|Toatl with additional charges|

--- chemtech_custom_app/public/purchase_invoice.js
+++ chemtech_custom_app/public/purchase_invoice.js
@@ -37,7 +37,6 @@ frappe.ui.form.on('Purchase Invoice', {
         let final_total = (frm.doc.total || 0) + custom_total_with_others_charges;
 
         // Set the calculated total in a new or existing field
-        
         frm.set_value('custom_total_with_others_charges',final_total); // Store the additional charges total
     }
 });
